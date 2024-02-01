26960a822bda243d48eb7f6408ec7f72e8b15ff1
Nagendra babu <pnagendra341@gmail.com>
2024-02-01 17:37:09 +0530

    added data validation files

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from mlproject_0_1 import logger
 from mlproject_0_1.pipeline.stage01_data_ingesion import DataIngestionTrainingPipeline
+from mlproject_0_1.pipeline.stage02_data_validation import DataValidationTrainingPipeline
 
 STAGE_NAME = " Data ingestion step"
 
@@ -7,6 +8,19 @@ if __name__ == "__main__":
     try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
+
+STAGE_NAME = " Data Validation step"
+
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataValidationTrainingPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
---
+++ src/mlproject_0_1/pipeline/stage02_data_validation.py
@@ -0,0 +1,28 @@
+from mlproject_0_1.config.configuration import ConfigurationManager
+from mlproject_0_1.components.data_validation import DataValiadtion
+from src.mlproject_0_1 import logger
+
+STAGE_NAME = " Data Validation step"
+
+
+class DataValidationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_validation_config = config.get_data_validation_config()
+        data_validation = DataValiadtion(config=data_validation_config)
+        data_validation.validate_all_columns()
+
+
+
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataValidationTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
