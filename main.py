e7fe229ddeca88b1d47174dada4366e70f20fe92
Nagendra babu <pnagendra341@gmail.com>
2024-02-01 19:15:07 +0530

    data transfermATION FILE ADDED

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from mlproject_0_1 import logger
 from mlproject_0_1.pipeline.stage01_data_ingesion import DataIngestionTrainingPipeline
 from mlproject_0_1.pipeline.stage02_data_validation import DataValidationTrainingPipeline
+from mlproject_0_1.pipeline.stage03_data_transfermation import DataTransformationTrainingPipeline
 
 STAGE_NAME = " Data ingestion step"
 
@@ -21,6 +22,18 @@ if __name__ == "__main__":
     try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         obj = DataValidationTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
+STAGE_NAME = "Data Transformation stage"
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataTransformationTrainingPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
